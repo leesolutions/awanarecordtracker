cad0ae30952cba6df8b403a7f67ee0ca5394316f
Dan Lee <dan@leesolutions.net>
2012-11-14 22:34:56 -0800

    grant all privileges to everyone on cancan ability; eliminate further unnecessary code in announcements controller and debug extra } from previous commit

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -1,32 +1,28 @@
 class AnnouncementsController < ApplicationController
 
+  load_and_authorize_resource
+
   def index
-    @announcements = Announcement.all
   end
 
   def show
-    @announcement = Announcement.find(params[:id])
   end
 
   def new
-    @announcement = Announcement.new
   end
 
   def edit
-    @announcement = Announcement.find(params[:id])
   end
 
   def create
-    @announcement = Announcement.new(params[:announcement])
     if @announcement.save
-      redirect_to @announcement, notice: 'Announcement was successfully created.' }
+      redirect_to @announcement, notice: 'Announcement was successfully created.'
     else
       render action: "new"
     end
   end
 
   def update
-    @announcement = Announcement.find(params[:id])
     if @announcement.update_attributes(params[:announcement])
       redirect_to @announcement, notice: 'Announcement was successfully updated.'
     else
@@ -35,7 +31,6 @@ class AnnouncementsController < ApplicationController
   end
 
   def destroy
-    @announcement = Announcement.find(params[:id])
     @announcement.destroy
     redirect_to announcements_url
   end
